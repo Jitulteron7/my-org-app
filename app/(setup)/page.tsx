03a5c193fba9e9346531210ede249347a3ddcea3
Jitulteron7 <jitulteron9@gmail.com>
2023-10-18 15:19:32 +0530

    create profile added

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,24 @@
+import { InitModal } from "@/components/modals/init-modal";
+import { db } from "@/lib/db";
+import { initProfile } from "@/lib/init-profile";
+import { redirect } from "next/navigation";
+
+const SetupPage = async () => {
+  const profile = await initProfile();
+  const server = await db.server.findFirst({
+    where: {
+      member: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+  return <InitModal />;
+};
+
+export default SetupPage;
---
+++ lib/init-profile.ts
@@ -0,0 +1,37 @@
+import { auth, currentUser, redirectToSignIn } from "@clerk/nextjs";
+
+import { db } from "@/lib/db";
+
+export const initProfile = async () => {
+  const { userId } = auth();
+
+  if (userId) {
+    // Query DB for user specific information or display assets only to logged in users
+  }
+
+  // Get the User object when you need access to the user's information
+  const user = await currentUser();
+  if (!user) {
+    return redirectToSignIn();
+  }
+
+  const profile = await db.profile.findUnique({
+    where: {
+      userId: user.id,
+    },
+  });
+
+  if (profile) {
+    return profile;
+  }
+
+  const newProfile = await db.profile.create({
+    data: {
+      userId: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress,
+    },
+  });
+  return newProfile;
+};
